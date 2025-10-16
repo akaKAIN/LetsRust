4ff8fff3ebf086a1cda52476b5ad66d0e66beea5
mkain <mkain@mail.ru>
2025-10-16 11:54:29 +0300

    task 2 + tests

--- simple/src/main.rs
+++ simple/src/main.rs
@@ -16,6 +16,30 @@ fn square(side_size: f32) -> (f32, f32, f32) {
     (side_size * 4.0, side_size * side_size, short_d)
 }
 
+/*
+Написать функцию season, принимающую 1 аргумент — номер
+месяца (от 1 до 12), и возвращающую время года, которому
+этот месяц принадлежит (зима, весна, лето или осень).
+ */
+fn season(month: u8) -> String {
+    let winter: &str = "зима";
+    let spring = "весна";
+    let summer = "лето";
+    let autumn = "осень";
+
+    if month == 12 || (month > 0 && month <= 2) {
+        return winter.to_string();
+    } else if month > 2 && month <= 5 {
+        return spring.to_string();
+    } else if month > 5 && month <= 8 {
+        return summer.to_string();
+    } else if month > 8 && month <= 11 {
+        return autumn.to_string();
+    };
+
+    return autumn.to_string();
+}
+
 
 
 #[cfg(test)]
@@ -26,6 +50,34 @@ mod tests {
     fn square_is_correct() {
         assert_eq!(square(2.0), (8.0, 4.0, 2.83));
         assert_eq!(square(4.0), (16.0, 16.0, 5.66))
+    }
+
+    #[test]
+    fn season_is_winter() {
+        assert_eq!(season(1), "зима".to_string());
+        assert_eq!(season(2), "зима".to_string());
+        assert_eq!(season(12), "зима".to_string());
+    }
+
+    #[test]
+    fn season_is_spring() {
+        assert_eq!(season(3), "весна".to_string());
+        assert_eq!(season(4), "весна".to_string());
+        assert_eq!(season(5), "весна".to_string());
+    }
+
+    #[test]
+    fn season_is_summer() {
+        assert_eq!(season(6), "лето".to_string());
+        assert_eq!(season(7), "лето".to_string());
+        assert_eq!(season(8), "лето".to_string());
+    }
+
+    #[test]
+    fn season_is_autumn() {
+        assert_eq!(season(9), "осень".to_string());
+        assert_eq!(season(10), "осень".to_string());
+        assert_eq!(season(11), "осень".to_string());
     }
 
 }
